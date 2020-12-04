5658f4a4fcfabfbf5af6f9359096fdee6317b537
dxz-d <1205760997@qq.com>
2020-12-05 07:25:20 +0800

    编写登录页面，联调后端登录接口

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
 
+// 设置反向代理，前端请求默认发送到http://localhost:8443/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 全局注册，之后可在其他组件中通过this.$axios 发送数据
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router"
+// 导入刚才编写的组件
+import Appindex from "@/components/home/Appindex";
+import Login from "@/components/Login";
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        // 下面都是固定的写法
+        {
+          path: '/',
+          redirect: '/login'
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/index',
+            name: 'Appindex',
+            component: Appindex
+        }
+    ]
+
+})
