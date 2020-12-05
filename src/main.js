56cfd7b5245ce224ff861001b3f3f6598aaf34df
dxz-d <1205760997@qq.com>
2020-12-05 21:27:16 +0800

    使用elementUI美化前端登录页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 设置反向代理，前端请求默认发送到http://localhost:8443/api
 var axios = require('axios')
@@ -9,8 +11,11 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
