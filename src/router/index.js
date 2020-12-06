046de29c1c3c2681b89ebb97dbacfb77a3c3ea9b
dxz-d <1205760997@qq.com>
2020-12-06 09:39:05 +0800

    导航栏的实现

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 // 导入刚才编写的组件
 import AppIndex from "@/components/home/Appindex";
 import Login from "@/components/Login";
+import Home from  "../components/Home"
 
 Vue.use(Router)
 
@@ -11,21 +12,26 @@ export default new Router({
     routes: [
         // 下面都是固定的写法
         {
-          path: '/',
-          redirect: '/login'
-        },
-        {
             path: '/login',
             name: 'Login',
             component: Login
         },
         {
-            path: '/index',
-            name: 'AppIndex',
-            component: AppIndex,
-            meta: {
-                requireAuth: true
-            }
+            path: '/home',
+            name: 'Home',
+            component: Home,
+            // Home页面不需要被访问
+            redirect: '/index',
+            children: [
+                {
+                    path: '/index',
+                    name: 'AppIndex',
+                    component: AppIndex,
+                    meta: {
+                        requireAuth: true
+                    }
+                }
+            ]
         }
     ]
 
